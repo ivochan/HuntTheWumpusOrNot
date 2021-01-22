0ca3226ff8e10bc41a8314c1d02b8ea825c586eb
ivonne <ivochan17@gmail.com>
2021-01-22 17:41:47 +0100

    predisposti i metodi di riempimento della mappa

--- src/game_structure/TestMappaGioco.java
+++ src/game_structure/TestMappaGioco.java
@@ -20,25 +20,27 @@ public class TestMappaGioco {
 			}
 		}
 		*/
+		
 		//creazione di una nuova mappa
 		GameMap mappa = new GameMap();
 		//vettore degli elementi di gioco
-		int [] elem_gioco= new int [8];
+		int [] elem_gioco= new int [9];
 		//riempimento del vettore
 		//riempiVettore(mappa,elem_gioco);
 		//stampa su console
 		//stampaVettore(elem_gioco);
 		
 		//ESEMPIO del vettore di gioco
-		//| [16] [16] [13]  [3]  [1]  [1]  [1]  [2]  [2] 
+		//| [16] [16] [13] [3] [3] [1] [1] [1] [2] [2] | 
 		elem_gioco[0]=16;//celle da riempire rimaste
 		elem_gioco[1]=16;//n_max celle
 		elem_gioco[2]=3;//sassi (celle non giocabili)
-		elem_gioco[3]=1;//oro
-		elem_gioco[4]=1;//eroe
-		elem_gioco[5]=1;//wumpus
-		elem_gioco[6]=2;//pozzi
-		elem_gioco[7]=2;//n_max pozzi
+		elem_gioco[3]=3;//numero massimo di sassi
+		elem_gioco[4]=1;//oro
+		elem_gioco[5]=1;//eroe
+		elem_gioco[6]=1;//wumpus
+		elem_gioco[7]=2;//pozzi
+		elem_gioco[8]=2;//n_max pozzi
 		stampaVettore(elem_gioco);
 		//stampa della mappa
 		//System.out.println(mappa);
@@ -51,21 +53,21 @@ public class TestMappaGioco {
 	
 	private static void stampaVettore(int[] elem_gioco) {
 		//si stampa una legenda
-		System.out.println("[celle]   [n_max celle]   [sassi]  [oro]"
-				+ "  [eroe] [wumpus] [pozzi] [n_max pozzi]\n");
+		System.out.println("[celle 0] [n_max celle 1] [sassi 2] [n_max sassi 3]"
+				+ " [oro 4] [eroe 5] [wumpus 6] [pozzi 7] [n_max pozzi 8]\n");
 		//si stampa il contenitore a sinistra
-		System.out.print("|");
+		System.out.print("| ");
 		//si scorrono le celle del vettore
 		for(int i=0;i<elem_gioco.length;i++) {
 			//si stampa la cella i-esima
 			if(i<elem_gioco.length-1) {
-				System.out.print(" ["+elem_gioco[i]+"]	   ");
+				System.out.print("["+elem_gioco[i]+"] ");
 			}
 			else {
-				System.out.print(" ["+elem_gioco[i]+"] ");
+				System.out.print("["+elem_gioco[i]+"] ");
 			}
 		}//for
-		System.out.println("|");
+		System.out.println("|\n");
 	}//stampaVettore
 
 	private static void popolaMappa(GameMap mappa, int [] elem_gioco) {
@@ -78,7 +80,7 @@ public class TestMappaGioco {
 				//si impsta il contenuto della cella
 				mappa.mappa_gioco[i][j] = new Cell(cs);
 				//si stampa il vettore degli elementi di gioco
-				//stampaVettore(elem_gioco);
+				stampaVettore(elem_gioco);
 			}//for colonne
 		}//for righe
 	}//popolaMappa
@@ -97,15 +99,17 @@ public class TestMappaGioco {
 		int d=(int)(Math.random()*5);//(da 0 a 5 escluso)
 		//celle non giocabili DENIED
 		elem_gioco[2]=d;
+		//massimo numero di celle non giocabili DENIED
+		elem_gioco[3]=d;
 		//oro 
 		int g=1;
-		elem_gioco[3]=g;
+		elem_gioco[4]=g;
 		//avventuriero 
 		int h=1;
-		elem_gioco[4]=h;
+		elem_gioco[5]=h;
 		//mostro 
 		int w=1;
-		elem_gioco[5]=w;
+		elem_gioco[6]=w;
 		//pozzi da mettere nella mappa (hero_side = true)
 		int p = 2; 
 		int max_p = 2;
@@ -116,13 +120,13 @@ public class TestMappaGioco {
 		//nel vettore 
 		if(mappa.getGameMode()) {
 			//hero_side
-			elem_gioco[6]=p;
-			elem_gioco[7]=max_p;
+			elem_gioco[7]=p;
+			elem_gioco[8]=max_p;
 		}
 		else {
 			//wumpus_side
-			elem_gioco[6]=t;
-			elem_gioco[7]=max_t;
+			elem_gioco[7]=t;
+			elem_gioco[8]=max_t;
 		}
 	}//riempiVettore
 
@@ -134,10 +138,11 @@ public class TestMappaGioco {
 		//confrontare la probabilita'
 		double seed = (Math.random());//numero tra 0 e 1 escluso
 		//HERO_SIDE
-		if(elem_gioco[6]!=0&&isPit(seed,elem_gioco[6],elem_gioco[7],elem_gioco[0], elem_gioco[1])) {
+		//POZZO
+		if(elem_gioco[7]>0 && isPit(seed,elem_gioco[7],elem_gioco[8],elem_gioco[0], elem_gioco[1])) {
 			/* -seed e' il valore di soglia con cui confrontare la probabilita' ottenuta
-			 * -x = elem_gioco[6] e' il numero di pozzi rimasti da posizionare;
-			 * -max_x = elem_gioco[7] e' il numero totale di pozzi;
+			 * -x = elem_gioco[7] e' il numero di pozzi rimasti da posizionare;
+			 * -max_x = elem_gioco[8] e' il numero totale di pozzi;
 			 * -n = elem_gioco[0] e' il numero di celle rimaste da rimepire;
 			 * -n_max = elem_gioco[1] e' il numero massimo di celle che costituiscono la mappa;
 			 */
@@ -146,53 +151,109 @@ public class TestMappaGioco {
 			//n -> una celle e' stata occupata percio' il numero di celle libere e' diminuito
 			elem_gioco[0]-=1; 
 			//p -> un pozzo e' stato assegnato, percio' il numero di pozzi da collocare e' diminuito
-			elem_gioco[6]-=1;
+			elem_gioco[7]-=1;
 			//si stampa l'attuale vettore degli elementi di gioco
 			//si assegna l'etichetta alla cella
 			return CellStatus.PIT;
 		}
-		else if(isWumpus(seed,elem_gioco[0],elem_gioco[4])) {
-			//nella cella e' stato messo il wumpus
+		//MOSTRO
+		else if(elem_gioco[6]>0 && isWumpus(seed,elem_gioco[6],elem_gioco[0], elem_gioco[1])) {
 			//TODO aggiornare il vettore dei sensori
+			//si aggiornano i parametri
+			elem_gioco[6]-=1;
+			//si riducono le celle libere
+			elem_gioco[0]-=1;
+			//nella cella e' stato messo il wumpus
 			return CellStatus.WUMPUS;
 		}
-		else if(isGold(seed,elem_gioco[0],elem_gioco[2])) {
+		else if(elem_gioco[4]>0 && isGold(seed,elem_gioco[4],elem_gioco[0],elem_gioco[1])) {
 			//e' stato messo l'oro
+			//so aggiornano i parametri
+			elem_gioco[4]-=1;
+			//si riduce il numero di celle occupate
+			elem_gioco[0]-=1;
 			return CellStatus.GOLD;
 		}
-		else if(isStone(seed,elem_gioco[0],elem_gioco[1])) {
-			//la cella e' DENIED
+		else if(elem_gioco[2]>0 && isStone(seed,elem_gioco[2],elem_gioco[0],elem_gioco[1])) {
+			//si aggiornano i parametri
+			elem_gioco[2]-=1;
+			//si riduce il numero delle celle libere
+			elem_gioco[0]-=1;
+			//la cella e' una PIETRA
 			return CellStatus.DENIED;
 		}
 		else {
-			System.out.println("Cella SAFE\n");
 			//allora e' una cella sicura
+			System.out.println("Cella SAFE\n");
 			//si aggiornano i parametri
-			elem_gioco[0]-=1;//numero celle da riempire
+			elem_gioco[0]-=1;
+			//numero celle da riempire
 			return CellStatus.SAFE;
 		}
-			//TODO manca il posizionamento dell'eroe
+		//TODO manca il posizionamento dell'eroe
 	}//scegliCella
 
-	private static boolean isStone(double seed, int i, int j) {
-		//i parametri sono il valore di soglia con cui 
-		//confrontare la probabilita' da calcolare
-		//il numero di pietre che ancora possono essere messe
-		//il numero massimo di pietre in generale
-		//si calcola la probabilita'della cella
-	
+	private static boolean isWumpus(double seed, int x, int n, int n_max) {
+		/*i parametri sono:
+		 *-il valore di soglia seed con cui confrontare la probabilita';
+		 *-l'oggetto wumpus x = elem_gioco[6];
+		 *-il numero massimo di wumpus x_max = 1;
+		 *-il numero di celle n da riempire = elem_gioco[0];
+		 *-il numero massimo di celle n_max = elem_gioco[1];
+		 */
+		double prob=lambda(x, x, n, n_max);
+		System.out.println("Probabilita' wumpus "+prob);
+		System.out.println("Soglia wumpus "+seed+"\n");
+		//confronto della probabilita' con il valore di soglia
+		if(prob>=seed) {
+			//allora si puo' posizionare il wumpus
+			System.out.println("Il Wumpus e' stato posizionato!\n");
+			return true;
+		}
+		//la cella non conterra' il mostro
 		return false;
-	}
+	}//isWumpus
 
-	private static boolean isGold(double seed, int i, int j) {
-		// TODO Auto-generated method stub
+	private static boolean isGold(double seed, int x, int n, int n_max) {
+		/*i parametri sono:
+		 *-il valore di soglia seed con cui confrontare la probabilita';
+		 *-l'oggetto oro x = elem_gioco[4];
+		 *-il numero massimo di gold x_max = 1;
+		 *-il numero di celle n da riempire = elem_gioco[0];
+		 *-il numero massimo di celle n_max = elem_gioco[1];
+		 */
+		double prob=lambda(x, x, n, n_max);
+		System.out.println("Probabilita' oro "+prob);
+		System.out.println("Soglia oro "+seed+"\n");
+		//confronto della probabilita' con il valore di soglia
+		if(prob<=seed) {
+			//allora si puo' posizionare l'oro
+			System.out.println("L'oro e' stato posizionato!\n");
+			return true;
+		}
+		//la cella non conterra' l'oro
 		return false;
-	}
+	}//isGold
 
-	private static boolean isWumpus(double seed, int i, int j) {
-		// TODO Auto-generated method stub
+	private static boolean isStone(double seed, int x, int n, int n_max) {
+		/*i parametri sono:
+		 *-il valore di soglia seed con cui confrontare la probabilita';
+		 *-l'oggetto SASSO -> DENIED x = elem_gioco[2];
+		 *-il numero massimo di sassi x_max = elem_gioco[3];
+		 *-il numero di celle n da riempire = elem_gioco[0];
+		 *-il numero massimo di celle n_max = elem_gioco[1];
+		 */
+		double prob=lambda(x, x, n, n_max);
+		System.out.println("Probabilita' pietra "+prob);
+		System.out.println("Soglia pietra "+seed+"\n");
+		//confronto della probabilita' con il valore di soglia
+		if(prob<=seed) {
+			//allora si puo' posizionare l'oro
+			System.out.println("Un sasso e' stato posizionato!\n");
+			return true;
+		}
 		return false;
-	}
+	}//isStone
 	
 	
 	private static boolean isPit(double seed, int x_pit, int x_max_pit, int n, int n_max) {
