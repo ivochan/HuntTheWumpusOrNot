80f5f20217f2860ebeb2307c01ee18a14082cb34
ivonne <ivochan17@gmail.com>
2021-02-08 12:32:26 +0100

    aggiornamento documentazione dei metodi che aggiornano il valore dei sensori

--- src/game_structure/GameMap.java
+++ src/game_structure/GameMap.java
@@ -541,7 +541,13 @@ public class GameMap {
 		}//while
 	}//populateMap
 
-	//TODO metodo per impostare i sensori
+	/** metodo sensorAssignement(): void
+	 * questo metodo si occupa si specificare il valore dei sensori per le celle che 
+	 * circoscrivono elementi di gioco di interesse, come il nemico o i pericoli.
+	 * Quindi, avendo a disposizione gli indici di riga e colonna in cui questi risiedono,
+	 * verra' poi specificato, nelle celle ad essi adiacenti, il valore che specifica
+	 * che tipo di elemento si trova nelle vicinanze, rispettando la modalita' di gioco.
+	 */
 	private void sensorAssignement() {
 		//si crea un vettore che conterra' gli indici di riga delle celle
 		//che contengono i pozzi o trappole
@@ -555,8 +561,20 @@ public class GameMap {
 		updateEnemySensors(enemy_indices);
 		//si specificano i valori dei sensori per gli indizi
 		updateDangerSensors(pit_trap_i, pit_trap_j);
-	}//defineSensors()
+	}//sensorAssignement()
 	
+	/** metodo updateEnemySensors(int []): void
+	 * questo metodo, dopo aver ricevuto in ingresso il vettore che contiene gli indici della
+	 * cella in cui e' stato posizionato il nemico, wumpus o avventuriero che sia, a seconda della
+	 * modalita' di gioco specificata al momento delle creazione della mappa, si occupa di impostare
+	 * il primo elemento del vettore dei sensori come "true", per ogni cella adiacente a questa.
+	 * @param enemy_indices: int[], e' il vettore che contiene l'indice di riga e l'indice di colonna
+	 * 								della cella in cui e' stato posizionato il nemico.
+	 * Nelle due modalita', si avra', rispettivamente:
+	 * -STINK = true, che indica l'odore del wumpus, se il mostro si trova nelle vicinanze (hero_side);
+	 * -SWISH = true, rappresenta il fruscio di foglie, causato dal nascondersi dell'avventuriero,
+	 * 				  per fare un agguato al wumpus (wumpus_side= !hero_side);
+	 */
 	private void updateEnemySensors(int[] enemy_indices) {
 		//si prelevano gli indici del nemico
 		int ie = enemy_indices[0];
@@ -564,40 +582,55 @@ public class GameMap {
 		//si controlla di non sforare la dimensione del vettore
 		//System.out.println("Il nemico e' nella cella "+ie+","+je);
 		//si specifica il vettore dei sensori per le celle attorno al nemico
-		//cella a sinistra
 		if(je>0) {
 			//DEBUG
 			//System.out.println("cella "+ie+","+(je-1));
 			//STINK o CREAK
 			game_map[ie][je-1].setSenseVectorCell(0,true);
 			//System.out.println(game_map[ie][je-1].senseVectorToString(hero_side));
-		}
-		//cella in alto
+		}// fi cella a SINISTRA
 		if(ie>0) {
 			//DEBUG
 			//System.out.println("cella "+(ie-1)+","+je);
 			//STINK o CREAK
 			game_map[ie-1][je].setSenseVectorCell(0,true);
 			//System.out.println(game_map[ie-1][je].senseVectorToString(hero_side));
-		}
-		//cella a destra
+		}//fi cella in ALTO
 		if(je<3) {
 			//DEBUG
 			//System.out.println("cella "+ie+","+(je+1));
 			//STINK o CREAK
 			game_map[ie][je+1].setSenseVectorCell(0,true);
 			//System.out.println(game_map[ie][je+1].senseVectorToString(hero_side));
-		}
-		//cella in basso
+		}//fi cella a DESTRA
 		if(ie<3) {
 			//DEBUG
 			//System.out.println("cella "+(ie+1)+","+je);
 			//STINK o CREAK
 			game_map[ie+1][je].setSenseVectorCell(0,true);
 			//System.out.println(game_map[ie+1][je].senseVectorToString(hero_side));
-		}
-	}
+		}//fi cella in BASSO
+	}//updateEnemySensors()
 
+	/** metodo updateDangerSensors(int [], int []): void
+	 * questo metodo, dopo aver ricevuto i vettori che contengono le posizioni in cui si
+	 * trovano i pericoli nella mappa di gioco, pozzi o trappole che siano, in base alla 
+	 * modalita' di gioco scelta, si occupa di aggiornare il valore del secondo elemento 
+	 * del vettore dei sensori, impostandolo pari a "true" (BREEZE o CREAK), per tutte le celle
+	 * che circoscrivono quella in cui si trova l'elemento di interesse, cioe' il pozzo o 
+	 * la trappola.
+	 * Rispettivamente, nelle due modalita', si avra':
+	 * 
+	 * -CREAK = true, rappresenta lo scricchiolio dei rami usati per camuffare la presenza di 
+	 * 				  una trappola, creata dall'avventuriero (wumpus_side = !hero_side);
+	 * -BREEZE = true, indica la brezza che si propaga nell'aria se nelle vicinanze si trova
+	 * 				   un pozzo in cui l'avventuriero rischia di cadere (hero_side);
+	 * 
+	 * @param pit_trap_i: int[], vettore che contiene gli indici riga delle celle che contengono
+	 * 							 gli elementi di pericolo;
+	 * @param pit_trap_j: int [], vettore che contiene gli indici colonna delle celle che 
+	 * 							  contengono gli elementi di pericolo;
+	 */
 	private void updateDangerSensors(int[] pit_trap_i, int[] pit_trap_j) {
 		//variabili ausiliarie per scorrere i vettori
 		int id=0;
@@ -642,11 +675,23 @@ public class GameMap {
 				System.out.println(game_map[id+1][jd].senseVectorToString(hero_side));
 			}
 		}//for
-		
 	}//updateDangerSensor
 	
-	
-		
+	/** metodo settingIndices(int[], int[], int[]):void
+	 * questo metodo, che riceve in ingresso i tre vettore che conterranno gli indici riga e 
+	 * colonna delle celle che conterranno il nemico ed i pericoli che dovra' affrontare il 
+	 * personaggio giocabile, si occupa di iterare tutta la mappa di gioco, andando a prelevare
+	 * proprio questi indici di interesse, per memorizzarli nei vettori ricevuti come parametro.
+	 * @param enemy_indices: int[], vettore che contiene la coppia di indici riga e colonna che
+	 * 								indica la posizione della cella che contiene il nemico nella
+	 * 								mappa di gioco;
+	 * @param pit_trap_i: int[], vettore che contiene gli indici riga di tutte le celle della mappa
+	 *							 di gioco che contengono un pericolo, cioe' un pozzo oppure una trappola,
+	 *							 in base alla modalita' in cui si sta giocando;
+	 * @param pit_trap_j: int[], vettore che contiene gli indici colonna di tutte le celle della mappa
+	 *							 di gioco che contengono un pericolo, cioe' un pozzo oppure una trappola,
+	 *							 in base alla modalita' in cui si sta giocando;
+	 */
 	private void settingIndices(int[] enemy_indices, int[] pit_trap_i, int[] pit_trap_j) {
 		//variabile ausiliaria
 		String cstatus = new String("");
@@ -667,7 +712,7 @@ public class GameMap {
 					enemy_indices[1]=j; //indice di colonna 
 					//DEBUG
 					//System.out.println(cstatus);
-				}//fi
+				}//fi WUMPUS
 				if(cstatus.equals("HERO") && !hero_side) {
 					//e' stato trovato l'eroe
 					//si prelevano gli indici di cella
@@ -675,7 +720,7 @@ public class GameMap {
 					enemy_indices[1]=j; //indice di colonna
 					//DEBUG
 					//System.out.println(cstatus);
-				}
+				}//fi AVVENTURIERO
 				if(cstatus.equals("PIT") && hero_side) {
 					//e' stata trovata una cella contenente il pozzo
 					pit_trap_i[i_pt] = i; //indice di riga
@@ -685,10 +730,10 @@ public class GameMap {
 						//si incrementano gli indici per accedere alla cella successiva
 						i_pt++;
 						j_pt++;
-					}
+					}//fi indici
 					//DEBUG
 					//System.out.println(cstatus);
-				}
+				}//fi POZZO
 				if(cstatus.equals("TRAP") && !hero_side) {
 					//e' stata trovata una cella contenente la trappola
 					pit_trap_i[i_pt] = i; //indice di riga
@@ -698,13 +743,12 @@ public class GameMap {
 						//si incrementano gli indici per accedere alla cella successiva
 						i_pt++;
 						j_pt++;
-					}
+					}//fi indici
 					//DEBUG
 					//System.out.println(cstatus);		
-				}
-			}
-		}
-		
+				}//fi TRAPPOLA
+			}//for colonne
+		}//for righe
 	}//settingIndices
 
 	/** metodo toString() : String
